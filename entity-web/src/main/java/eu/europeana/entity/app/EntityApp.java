9e7cd6b2d14c351b7881f713ca54dfd3c560bbb4
srishti <srishti.singh@europeana.eu>
2023-06-26 11:46:28 +0200

    EA-3473 remove socks proxy

--- entity-web/src/main/java/eu/europeana/entity/app/EntityApp.java
+++ entity-web/src/main/java/eu/europeana/entity/app/EntityApp.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import eu.europeana.entity.web.config.EntitySocksProxyActivator;
-
 /**
  * Main application. Allows deploying as a war and logs instance data when
  * deployed in Cloud Foundry
@@ -28,20 +26,6 @@ public class EntityApp extends SpringBootServletInitializer {
      */
     public static void main(String[] args) {
         LogManager.getLogger(EntityApp.class).info("Configure Spring Application!");
-        // When deploying to Cloud Foundry, this will log the instance index number, IP
-        // and GUID
-        LogManager.getLogger(EntityApp.class).info(
-                "CF_INSTANCE_INDEX  = {}, CF_INSTANCE_GUID = {}, CF_INSTANCE_IP  = {}",
-                System.getenv("CF_INSTANCE_INDEX"), System.getenv("CF_INSTANCE_GUID"), System.getenv("CF_INSTANCE_IP"));
-
-        registerSocksProxy();
         SpringApplication.run(EntityApp.class, args);
     }
-
-    protected static void registerSocksProxy() {
-        // Activate socks proxy (if your application requires it)
-        LogManager.getLogger(EntityApp.class).info("Configure SocksProxy...");
-        EntitySocksProxyActivator.activate(new SocksProxyConfig("entity.properties", "entity.user.properties"));
-    }
-
 }
